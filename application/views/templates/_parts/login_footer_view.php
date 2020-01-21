020403e23fff923f41c554bd6830b31ca02f3550
michaeloncode <michaeloncode@gmail.com>
2020-01-21 16:19:31 +0100

    Latest Modification of the day

--- application/views/templates/_parts/login_footer_view.php
+++ application/views/templates/_parts/login_footer_view.php
@@ -11,6 +11,5 @@ if (isset($clientData) && !empty($clientData)) {
 <script src="<?= $assetsUrl ?>pro/js/app.min.js"></script>
 <script src="<?= $assetsUrl ?>pro/js/public.js?v=1.003"></script>
 <script src="<?= $assetsUrl ?>public/js/ajaxify.js?v=1.001"></script>
-
 </body>
 </html>
